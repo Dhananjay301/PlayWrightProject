fceb8eb840603c0be5b02c75f707b64f763edd9b
admin <admin@DESKTOP-FOTJODL>
2025-07-09 14:46:11 +0530

    Browser Context

---
+++ src/test/java/browserContext/BrowserContextTC.java
@@ -0,0 +1,42 @@
+package browserContext;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+
+
+
+
+
+
+public class BrowserContextTC {
+
+	
+	public static void main(String[] args) {
+		//to open 2browsers
+		
+		Playwright playwright=Playwright.create();
+		Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		BrowserContext br1=browser.newContext();
+		Page page1=br1.newPage();
+		page1.navigate("https://www.flipkart.com/");
+		String title1=page1.title();
+		System.out.println(title1);
+		
+		BrowserContext br2=browser.newContext();
+		Page page2=br2.newPage();
+		page2.navigate("https://www.amazon.com/ref=nav_logo");
+		String title2=page2.title();
+		System.out.println(title2);
+		 
+		page1.close();
+		br1.close();
+		br2.close();
+		page2.close();
+
+	}
+
+}
